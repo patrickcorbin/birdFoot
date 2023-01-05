791d2157840cbedd1375accdbe1ba366f824fb2e
patrickcorbin <64284972+patrickcorbin@users.noreply.github.com>
2023-01-05 14:44:10 -0600

    Google Maps testing

---
+++ src/data/locations.js
@@ -0,0 +1,29 @@
+const locations = [
+    {
+        lat: 29.965267525562698,
+        lng: -90.06470307116393,
+        title: 'George and Joyce Wein Jazz & Heritage Center',
+        description: 'Faubourg Tremé'
+    },
+    {
+        lat: 29.944007133511583,
+        lng: -90.07075213672194,
+        title: 'Contemporary Arts Center New Orleans',
+        description: 'Downtown'
+    },
+    {
+        lat: 29.94852690357204,
+        lng: -90.07183421581978,
+        title: 'Ace Hotel New Orleans',
+        description: 'Downtown'
+    },
+    {
+        lat: 29.95015420037682,
+        lng: -90.06964451581979,
+        title: '601 Poydras Street',
+        description: 'Downtown'
+    }
+]
+
+
+export { locations }
--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -1,8 +1,60 @@
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonViewWillEnter } from '@ionic/react';
+import { useRef } from 'react';
 import './Locations.css';
+import { GoogleMap } from '@capacitor/google-maps';
+import { locations } from '../data/locations.js';
 
 const Locations: React.FC = () => {
+
+  const key = "AIzaSyC_Apog-yi-mdlHVeD8PpAQETXskNFx23c";
+  let newMap: GoogleMap
+  const mapRef = useRef<HTMLElement>()
+
+  // const [mapConfig, setMapConfig] = useState({
+  //   center: {
+  //     lat: locations[0].lat,
+  //     lng: locations[0].lng
+  //   },
+  //   zoom: 12
+  // })
+
+  // const testKey = process.env.MY_MAPS_API_KEY
   
+  const createMap = async () => {
+
+    if (!mapRef.current) return
+
+    newMap = await GoogleMap.create({
+      id: "google-map",
+      element: mapRef.current,
+      apiKey: key,
+      config: {
+        center: {
+          lat: locations[0].lat,
+          lng: locations[0].lng
+        },
+        zoom: 12
+      }
+    })
+
+    addMarkers()
+  }
+
+  const addMapMarker = async (marker: any) => {
+    
+    await newMap.addMarker({
+      coordinate: {
+        lat: marker.lat,
+        lng: marker.lng
+      },
+      title: marker.title
+    })
+  }
+
+  const addMarkers = () => locations.forEach(marker => addMapMarker(marker))
+
+  useIonViewWillEnter(() => createMap());
+
     return (
       <IonPage className="demo-body">
         <IonHeader>
@@ -19,9 +71,14 @@ const Locations: React.FC = () => {
               <IonTitle size="large">Locations</IonTitle>
             </IonToolbar>
           </IonHeader>
-          <div className="location-container">
-            
-          </div>
+          
+          {/* <div className="location-container"> */}
+            <capacitor-google-map 
+              ref={mapRef} 
+              id="map"
+              className="test-map"
+            ></capacitor-google-map>
+          {/* </div> */}
         </IonContent>
       </IonPage>
     );
