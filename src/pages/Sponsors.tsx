deca62121099ec91fc76ec27fe41eb24b9a84d1c
patrickcorbin <64284972+patrickcorbin@users.noreply.github.com>
2023-02-16 14:30:16 -0600

    Sponsors update

--- src/pages/Sponsors.tsx
+++ src/pages/Sponsors.tsx
@@ -20,50 +20,52 @@ const Sponsors: React.FC = () => {
           </IonToolbar>
         </IonHeader> */}
         <div className="sponsors-container">
+            {/* <h2 className="full-row">Our Sponsors</h2> */}
             <a target="_blank" rel="noopener noreferrer" href="https://www.gnof.org/program/the-selley-foundation/" className="full-row">
                 <img className="sponsors__img" src={`./assets/images/sponsors/Selley_GNOF.png`} alt="Selley Foundation" />
-            </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.brennansneworleans.com/">
-                <img className="sponsors__img" src={`./assets/images/sponsors/Brennans.png`} alt="Brennans" />
-            </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.nojazzfest.com/">
-                <img className="sponsors__img" src={`./assets/images/sponsors/NOJHF.png`} alt="Jazz Fest" />
             </a>
             <a target="_blank" rel="noopener noreferrer" href="https://www.joneswalker.com/">
                 <img className="sponsors__img" src={`./assets/images/sponsors/JonesWalker.png`} alt="Jones Walker" />
             </a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.arts.gov/">
+                <img className="sponsors__img" src={`./assets/images/sponsors/NEA.png`} alt="National Endowment of the Arts" />
+            </a>
             <a target="_blank" rel="noopener noreferrer" href="https://www.crt.state.la.us/cultural-development/">
                 <img className="sponsors__img" src={`./assets/images/sponsors/LA-Div-Arts.png`} alt="LA Division of the Arts" />
             </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://nola.gov/">
-                <img className="sponsors__img" src={`./assets/images/sponsors/CityOfNO.png`} alt="City of New Orleans" />
+            <a target="_blank" rel="noopener noreferrer" href="https://www.nojazzfest.com/">
+                <img className="sponsors__img" src={`./assets/images/sponsors/NOJHF.png`} alt="Jazz Fest" />
             </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.arts.gov/">
-                <img className="sponsors__img" src={`./assets/images/sponsors/NEA.png`} alt="National Endowment of the Arts" />
+            <a target="_blank" rel="noopener noreferrer" href="https://www.wwno.org/">
+                <img className="sponsors__img" src={`./assets/images/sponsors/NOPR_TwoLogos.jpg`} alt="WWNO 89.9 FM" />
+            </a>
+            <a target="_blank" rel="noopener noreferrer" href="https://cacno.org/">
+                <img className="sponsors__img" src={`./assets/images/sponsors/logo-cac.jpg`} alt="Contemporary Arts Center" />
+            </a>
+            <a target="_blank" rel="noopener noreferrer" href="https://fertel.com/about-the-ruth-u-fertel-foundation/">
+                <img className="sponsors__img" src={`./assets/images/sponsors/Ruth-U-Fertel-Found.jpeg`} alt="Ruth U Fertel Foundation" />
             </a>
             <a target="_blank" rel="noopener noreferrer" href="https://www.hearhearnola.org/">
                 <img className="sponsors__img" src={`./assets/images/sponsors/HearHearNOLA.png`} alt="Hear Hear NOLA" />
             </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.wwno.org/classical-1049">
-                <img className="sponsors__img" src={`./assets/images/sponsors/Classical1049.png`} alt="Classical 104.9 FM" />
+            <a target="_blank" rel="noopener noreferrer" href="https://www.theriversretreat.com/">
+                <img className="sponsors__img" src={`./assets/images/sponsors/Rivers-Retreat.png`} alt="Rivers Retreat" />
             </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.wwno.org/">
-                <img className="sponsors__img" src={`./assets/images/sponsors/WWNO.png`} alt="WWNO 89.9 FM" />
+            <a target="_blank" rel="noopener noreferrer" href="https://www.theokrainn.com/">
+                <img className="sponsors__img" src={`./assets/images/sponsors/Okra-Inn.jpg`} alt="The Okra Inn" />
             </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.cafecarmo.com/">
-                <img className="sponsors__img" src={`./assets/images/sponsors/carmo.png`} alt="Cafe Carmo" />
+            <h2 className="full-row">Community Partners</h2>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.thedescendantsproject.com/">
+                <img className="sponsors__img" src={`./assets/images/sponsors/Desc-Project.jpeg`} alt="The Descendants Project" />
             </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://steinsdeli.com/">
-                <img className="sponsors__img" src={`./assets/images/sponsors/Steins.png`} alt="Steins Market &Deli" />
+            <a target="_blank" rel="noopener noreferrer" href="https://noma.org/">
+                <img className="sponsors__img" src={`./assets/images/sponsors/NOMA.jpg`} alt="NOMA" />
             </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://urbanearthstudios.com/">
-                <img className="sponsors__img" src={`./assets/images/sponsors/UrbanEarth.png`} alt="Urban Earth Design Studios" />
+            <a target="_blank" rel="noopener noreferrer" href="https://marignyoperahouse.org/">
+                <img className="sponsors__img" src={`./assets/images/sponsors/Marigny-Opera-House-logo.png`} alt="Marigny Opera House" />
             </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.themilkbarneworleans.com/">
-                <img className="sponsors__img" src={`./assets/images/sponsors/TheMilkBar.png`} alt="The Milk Bar" />
-            </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://stjamescheese.com/pages/full-menu-uptown">
-                <img className="sponsors__img" src={`./assets/images/sponsors/StJamesCheese.png`} alt="St. James Cheese Companytent" />
+            <a target="_blank" rel="noopener noreferrer" href="https://materialinstitute.org/">
+                <img className="sponsors__img" src={`./assets/images/sponsors/material_institute-logotype.png`} alt="Material Institute" />
             </a>
         </div>
       </IonContent>
