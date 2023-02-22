d992620d0624597612d9fded04824f7001622da9
patrickcorbin <64284972+patrickcorbin@users.noreply.github.com>
2023-02-22 10:16:52 -0600

    IDs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.birdFoot',
   appName: 'birdFoot',
   webDir: 'build',
   bundledWebRuntime: false
