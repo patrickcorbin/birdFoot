43b3e88a12d6a2c03720f7ad68259c44b6e00e95
patrickcorbin <64284972+patrickcorbin@users.noreply.github.com>
2023-02-22 12:37:58 -0600

    iOS Ready

--- src/hooks/useMaps.ts
+++ src/hooks/useMaps.ts
@@ -6,8 +6,10 @@ import { GoogleMap } from '@capacitor/google-maps';
 
 export function useMaps(locs: any) {
 
-    const key = (process.env.REACT_APP_MAP_KEY as string)
+    // const key = (process.env.REACT_APP_MAP_KEY as string)
     // const key = (process.env.REACT_APP_TEST_KEY as string) 
+    const key = (process.env.REACT_APP_MAP_KEY_IOS as string)
+    // const key = (process.env.REACT_APP_MAP_KEY_ANDROID as string)
 
     const [mapConfig, setMapConfig] = useState({
         center: {
